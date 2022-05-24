4f7334b13ad88ce68447510a2bedd5e72c733990
Sedki-MAGDOULI <magdouli.s@decade.fr>
2022-05-24 13:03:00 +0200

    first fix dependencies injection

--- src/DependencyInjection/BatchJobsExtension.php
+++ src/DependencyInjection/BatchJobsExtension.php
@@ -3,13 +3,24 @@
 
 namespace BatchJobs\BatchJobsBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class BatchJobsExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-       // var_dump("we are alive");die;
-     }
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
+        $configuration = $this->getConfiguration($configs, $container);
+        $this->processConfiguration($configuration, $configs);
+    }
+
+    public function getAlias()
+    {
+        return 'batch_jobs';
+    }
 }
